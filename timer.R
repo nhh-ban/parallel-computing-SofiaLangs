5b53b11f254d1c8328ee1e7f239a5287b9f98af8
SofiaLangs <sofia.langs@student.nhh.no>
2023-10-30 19:28:54 +0100

    Problem 2

---
+++ timer.R
@@ -0,0 +1,58 @@
+library(tidyverse)
+library(tictoc)
+library(doParallel)
+library(foreach)
+library(tweedie)
+library(furrr)
+library(future)
+#I use the function seen in class to store the different times and 
+#compare the results
+printTicTocLog <-
+  function() {
+    tic.log() %>%
+      unlist %>%
+      tibble(logvals = .) %>%
+      separate(logvals,
+               sep = ":",
+               into = c("Function type", "log")) %>%
+      mutate(log = str_trim(log)) %>%
+      separate(log,
+               sep = " ",
+               into = c("Seconds"),
+               extra = "drop")
+  }
+
+printTicTocLog() %>%
+  knitr::kable()
+
+#clear history
+tic.clearlog()
+
+############## Regular loop using solution as given
+tic("Regular loop")
+source("scripts/Regular_loop.R")
+toc(log = TRUE )
+printTicTocLog() %>% 
+  knitr::kable()
+
+##############Rewriting using furrr
+tic(paste0("Furrr loop, ",Cores, " cores") )
+source("scripts/furrr_loop.R")
+toc(log = TRUE )
+printTicTocLog() %>% 
+  knitr::kable()
+##############Parallel loop using more cores
+maxcores<- 6
+Cores<-min(parallel::detectCores(), maxcores) 
+
+tic(paste0("Parallel loop, ",Cores, " cores") )
+source("scripts/Parallel_loop.R")
+toc(log = TRUE )
+printTicTocLog() %>% 
+  knitr::kable()
+
+#Overall it appears that the fastest loop is the last, namely the parallel loop 
+#In class however we saw an example where furrr loop was faster so it may depend 
+#on the computer used or/and other activities/pages open on the computer
+
+#also for furrr I had to insert another if function that maybe slows the process
